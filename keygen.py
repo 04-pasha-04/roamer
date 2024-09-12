5bc01bd26d0b03de4f7d5a94eb197ee8239765ca
04-pasha-04 <pasha.syd04@gmail.com>
2024-09-12 19:53:23 +0200

    edit keygen to write to config.json

--- keygen.py
+++ keygen.py
@@ -1,22 +1,26 @@
 import secrets
 import os
+import json
 
-# Define the path where the token will be stored
-token_file_path = 'token.txt'
+# Load configuration from JSON file
+config_file_path = 'config.json'
+with open(config_file_path, 'r') as config_file:
+    config = json.load(config_file)
+
 
 def generate_and_store_token():
     # Generate a secure token
     token = secrets.token_hex(16)  # Generates a 32-character hex token
     
     # Store the token in a file
-    with open(token_file_path, 'w') as token_file:
-        token_file.write(token)
+    with open(config_file_path, 'w') as token_:
+        token_.write(token)
     
-    print(f"Token generated and stored in {token_file_path}")
+    print(f"Token generated and stored in {config_file_path}")
 
 if __name__ == "__main__":
     # Check if token file already exists
-    if os.path.exists(token_file_path):
-        print(f"Token file already exists at {token_file_path}.")
+    if os.path.exists(config_file_path):
+        print(f"Token file already exists at {config_file_path}.")
     else:
         generate_and_store_token()
